eaf4f5e98c8c3e3f3ecbd0ada51b429ae19eb633
黄小朵 <huang_ze@outlook.com>
2025-01-07 14:35:38 +0800

    feat：增加定时器轮训设备是否上线、停止定时器方法

--- platforms/ios/SmartHome_Common/WifiConfigure/DeviceWifiConfigure.m
+++ platforms/ios/SmartHome_Common/WifiConfigure/DeviceWifiConfigure.m
@@ -7,6 +7,14 @@
 
 #import "DeviceWifiConfigure.h"
 #import <SystemConfiguration/CaptiveNetwork.h>
+#import "DeviceOnlineApi.h"
+
+@interface DeviceWifiConfigure ()
+/// 定时器，调接口查询设备是否在线
+@property (nonatomic) dispatch_source_t onlineTimer;
+/// 定时器是否开启
+@property (nonatomic) BOOL onlineTimerStarted;
+@end
 
 @implementation DeviceWifiConfigure
 
@@ -21,6 +29,33 @@
     // 默认实现抛出异常
     [NSException raise:NSInternalInconsistencyException
                 format:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)];
+
+#pragma mark - Start & Stop Timer
+- (void)startQueryOnlineTimer {
+    if (self.onlineTimerStarted) {
+        return; // 如果定时器已经启动，则不重复启动
+    }
+
+    self.onlineTimerStarted = YES;
+    
+    // 设置定时器触发的间隔为 1 秒
+    dispatch_source_set_timer(self.onlineTimer, DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(self.onlineTimer, ^{
+        [self queryDeviceOnline];
+    });
+
+    // 启动定时器
+    dispatch_resume(self.onlineTimer);
+}
+
+- (void)stopQueryOnlineTimer {
+    if (self.onlineTimer && self.onlineTimerStarted) {
+        dispatch_source_cancel(self.onlineTimer);
+        self.onlineTimer = nil;
+        self.onlineTimerStarted = NO;
+    }
+}
+
 }
 
 + (NSString *)currentSSID {
@@ -34,4 +69,10 @@
     return wifiName;
 }
 
+
+#pragma mark - Property
+
+- (dispatch_source_t)onlineTimer {
+    return _onlineTimer ?: (_onlineTimer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)));
+}
 @end
